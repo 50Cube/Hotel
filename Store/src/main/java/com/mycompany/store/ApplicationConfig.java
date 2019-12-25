e6ac0c1c49fc31ef85f28820691c03c6d1c98d4e
GabroVV <origingabriel3124@gmail.com>
2019-12-25 23:30:42 +0100

    Jednak inaczej trzeba :))))))))

---
+++ Store/src/main/java/com/mycompany/store/ApplicationConfig.java
@@ -0,0 +1,18 @@
+package com.mycompany.store;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+import javax.security.enterprise.authentication.mechanism.http.CustomFormAuthenticationMechanismDefinition;
+import javax.security.enterprise.authentication.mechanism.http.LoginToContinue;
+
+
+@CustomFormAuthenticationMechanismDefinition(
+        loginToContinue = @LoginToContinue(
+                loginPage = "/login.html",
+                useForwardToLogin = false
+            )
+)
+@FacesConfig
+@ApplicationScoped
+public class ApplicationConfig {
+}
