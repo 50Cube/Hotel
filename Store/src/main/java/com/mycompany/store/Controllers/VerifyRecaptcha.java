dfd71ba6d5364b2398d6165609180fa8e2e32716
GabroVV <origingabriel3124@gmail.com>
2020-01-15 22:53:40 +0100

    Odhinduszanie

--- Store/src/main/java/com/mycompany/store/Controllers/VerifyRecaptcha.java
+++ Store/src/main/java/com/mycompany/store/Controllers/VerifyRecaptcha.java
@@ -1,59 +1,46 @@
 package com.mycompany.store.Controllers;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.StringReader;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL; 
 import javax.json.Json; 
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.net.ssl.HttpsURLConnection;
+
 public class VerifyRecaptcha {
-	public static final String url = "https://www.google.com/recaptcha/api/siteverify";
-	public static final String secret = "6LcdPsoUAAAAAIQzVOYS_90-liYwnud4MNc-ILZF";
+	public static final String URL = "https://www.google.com/recaptcha/api/siteverify";
+	public static final String SECRET = "6LcdPsoUAAAAAIQzVOYS_90-liYwnud4MNc-ILZF";
 	private final static String USER_AGENT = "Mozilla/5.0";
 	public static boolean verify(String gRecaptchaResponse) throws IOException {
 		if (gRecaptchaResponse == null || "".equals(gRecaptchaResponse)) {
 			return false;
 		}
-		try{
-		URL obj = new URL(url);
-		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
-		// add reuqest header
-		con.setRequestMethod("POST");
-		con.setRequestProperty("User-Agent", USER_AGENT);
-		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-		String postParams = "secret=" + secret + "&response="
+		URL obj = new URL(URL);
+		HttpsURLConnection conn = (HttpsURLConnection) obj.openConnection();
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("User-Agent", USER_AGENT);
+		conn.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+		String postParams = "secret=" + SECRET + "&response="
 				+ gRecaptchaResponse;
-		// Send post request
-		con.setDoOutput(true);
-		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-		wr.writeBytes(postParams);
-		wr.flush();
-		wr.close();
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'POST' request to URL : " + url);
-		System.out.println("Post parameters : " + postParams);
-		System.out.println("Response Code : " + responseCode);
-		BufferedReader in = new BufferedReader(new InputStreamReader(
-				con.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
-		// print result
-		System.out.println(response.toString());
-		//parse JSON response and return 'success' value
-		JsonReader jsonReader = Json.createReader(new StringReader(response.toString()));
-		JsonObject jsonObject = jsonReader.readObject();
-		jsonReader.close();
+		conn.setDoOutput(true);
+                
+                try (OutputStream outStream = conn.getOutputStream()) {
+                    outStream.write(postParams.getBytes());
+                    outStream.flush();
+                }
+                catch(IOException ioe){
+                    return false;
+                }
+
+                InputStream is = conn.getInputStream();
+                JsonObject jsonObject;
+            try (JsonReader jsonReader = Json.createReader(is)) {
+                jsonObject = jsonReader.readObject();
+            }
+            catch(RuntimeException re){
+                return false;
+            }
 		return jsonObject.getBoolean("success");
-		}catch(Exception e){
-			e.printStackTrace();
-			return false;
-		}
 	}
 }
